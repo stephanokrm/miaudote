44d13a1fbdea146acb043c3da6e76df92c71ba6b
Stephano Ramos <stephano.ramos.p@gmail.com>
2022-10-07 09:14:35 -0300

    refactors

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,11 @@ export async function middleware(request: NextRequest) {
         return NextResponse.next();
     }
 
-    return NextResponse.redirect(new URL('/login', request.url));
+    if (!authenticated) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    return NextResponse.next();
 }
 
 export const config = {

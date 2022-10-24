9a128c50f5ea8cad35d6d93083af4c3c47425ded
Stephano Ramos <stephano.ramos.p@gmail.com>
2022-10-24 17:46:11 -0300

    user edit form

--- pages/user/edit.tsx
+++ pages/user/edit.tsx
@@ -1,287 +1,39 @@
-import {GetServerSideProps, NextPage} from "next";
-import LoadingButton from '@mui/lab/LoadingButton';
-import Box from "@mui/material/Box";
-import Autocomplete from "@mui/material/Autocomplete";
-import Container from "@mui/material/Container";
-import Alert from "@mui/material/Alert";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import PetsIcon from '@mui/icons-material/Pets';
-import Head from "next/head";
-import {DatePicker} from "@mui/x-date-pickers/DatePicker";
-import {Controller} from "react-hook-form";
-import * as yup from "yup";
-import {format, parseISO, subYears} from 'date-fns';
-import {ChangeEvent} from "react";
-import {AsYouType} from "libphonenumber-js";
-import useForm from "../../src/hooks/useForm";
-import {User, UserUpdateFieldValues} from "../../src/types";
-import getUserByMe from "../../src/services/getUserByMe";
-import getStates from "../../src/services/getStates";
-import {AvatarChangeEvent, InteractableAvatar} from "../../src/components/InteractableAvatar";
-import {useGetCitiesByStateQuery} from "../../src/hooks/queries/useGetCitiesByStateQuery";
-import {useUserUpdateMutation} from "../../src/hooks/mutations/useUserUpdateMutation";
-import {dehydrate, QueryClient} from "@tanstack/react-query";
-import {useGetStatesQuery} from "../../src/hooks/queries/useGetStatesQuery";
+import {NextPage} from 'next';
+import Head from 'next/head';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import CircularProgress from '@mui/material/CircularProgress';
+import {useGetUserByMeQuery} from '../../src/hooks/queries/useGetUserByMeQuery';
+import {UserEditForm} from '../../src/components/UserEditForm';
 
-const minDate = subYears(new Date(), 150);
-const maxDate = subYears(new Date(), 18);
-const stateObject = yup.object({
-    name: yup.string().required(),
-    initials: yup.string().required(),
-    label: yup.string().required(),
-});
-const schema = yup.object({
-    name: yup.string().required('O campo nome é obrigatório.'),
-    bornAt: yup.date().required('O campo data de nascimento é obrigatório.').min(minDate, 'O campo data de nascimento deve ser maior que ' + format(minDate, 'dd/MM/yyyy') + '.').max(maxDate, 'A sua idade deve ser maior que 18 anos.'),
-    email: yup.string().email('O campo e-mail deve ser um endereço de e-mail válido.').required('O campo e-mail é obrigatório.'),
-    phone: yup.string().required('O campo celular é obrigatório.'),
-    city: yup.object({
-        id: yup.number().required(),
-        name: yup.string().required(),
-        label: yup.string().required(),
-        state: stateObject,
-    }).nullable().required('O campo cidade é obrigatório.'),
-})
+const UserShow: NextPage = () => {
+  const {data: user, isLoading: loadingUser} = useGetUserByMeQuery();
 
-type UserShowProps = {
-    user: User,
-}
-
-export const getServerSideProps: GetServerSideProps = async ({req}) => {
-    const queryClient = new QueryClient()
-
-    await queryClient.prefetchQuery(['getStates'], () => getStates());
-
-    return {
-        props: {
-            user: await getUserByMe({authorization: req.cookies.authorization}),
-            dehydratedState: dehydrate(queryClient),
-        },
-    }
-}
-
-const UserShow: NextPage<UserShowProps> = ({user}: UserShowProps) => {
-    const {
-        control,
-        handleSubmit,
-        formState: {errors},
-        setValue,
-        getValues,
-        setError,
-        trigger,
-        watch,
-    } = useForm<UserUpdateFieldValues>({
-        // @ts-ignore
-        schema,
-        defaultValues: {
-            ...user,
-            bornAt: parseISO(user.bornAtISO),
-        },
-    });
-    const {data: states} = useGetStatesQuery();
-    const {mutate: updateUser, message, isLoading: isUpdatingUser} = useUserUpdateMutation({setError});
-    const onSubmit = handleSubmit((data: UserUpdateFieldValues) => updateUser(data));
-    const {
-        data: cities,
-        isLoading: isLoadingCities,
-        refetch: refetchCities
-    } = useGetCitiesByStateQuery({state: getValues('city.state')});
-
-    const onAvatarChange = async ({file, avatar}: AvatarChangeEvent) => {
-        if (!file || !avatar) {
-            return setError('avatar', {
-                message: 'Invalid upload',
-            });
-        }
-
-        setValue('avatar', avatar);
-        setValue('file', file);
-
-        await trigger(['avatar', 'file']);
-    }
-
-    return (
-        <>
-            <Head>
-                <title>MiAudote - {watch('name')}</title>
-            </Head>
-            <Container maxWidth="sm">
-                <Box paddingY={3}>
-                    <Grid container justifyContent="center" alignContent="center" spacing={2}>
-                        <Grid item>
-                            <Card>
-                                <CardContent>
-                                    <form onSubmit={onSubmit}>
-                                        <Grid container spacing={2} justifyContent="center">
-                                            <Grid item xs={12} textAlign="center">
-                                                <PetsIcon fontSize="large" color="primary"/>
-                                            </Grid>
-                                            <Grid item xs={12} justifyContent="center" display="flex">
-                                                <InteractableAvatar onChange={onAvatarChange} alt={watch('name')}
-                                                                    src={getValues('avatar')}/>
-                                            </Grid>
-                                            {message && (
-                                                <Grid item xs={12}>
-                                                    <Alert severity="error">{message}</Alert>
-                                                </Grid>
-                                            )}
-                                            <Grid item xs={12}>
-                                                <Typography variant="h5">Dados Pessoais</Typography>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Controller
-                                                    name="name"
-                                                    control={control}
-                                                    render={({field}) => <TextField {...field} label="Nome"
-                                                                                    variant="filled"
-                                                                                    fullWidth error={!!errors.name}
-                                                                                    helperText={errors.name?.message}/>}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Controller
-                                                    name="bornAt"
-                                                    control={control}
-                                                    render={({field}) => {
-                                                        return <DatePicker
-                                                            label="Data de Nascimento"
-                                                            inputFormat="dd/MM/yyyy"
-                                                            value={getValues('bornAt')}
-                                                            onChange={(bornAt) => {
-                                                                if (bornAt) setValue('bornAt', bornAt);
-                                                            }}
-                                                            disableFuture
-                                                            openTo="year"
-                                                            views={['year', 'month', 'day']}
-                                                            minDate={minDate}
-                                                            maxDate={maxDate}
-                                                            renderInput={(params) => {
-                                                                const inputProps = {
-                                                                    ...field,
-                                                                    ...params.inputProps,
-                                                                };
-
-                                                                return (
-                                                                    <TextField {...params}
-                                                                               fullWidth
-                                                                               inputProps={inputProps}
-                                                                               variant="filled"
-                                                                               error={!!errors.bornAt}
-                                                                               helperText={errors.bornAt?.message}/>
-                                                                )
-                                                            }}
-                                                        />
-                                                    }}/>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Controller
-                                                    name="phone"
-                                                    control={control}
-                                                    render={({field}) => (
-                                                        <TextField
-                                                            {...field}
-                                                            label="Celular"
-                                                            variant="filled"
-                                                            fullWidth
-                                                            error={!!errors.phone}
-                                                            helperText={errors.phone?.message}
-                                                            onChange={(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-                                                                field.onChange(new AsYouType('BR').input(event.target.value));
-                                                            }}
-                                                        />
-                                                    )}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Typography variant="h5">Endereço</Typography>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Autocomplete
-                                                    value={getValues('city.state') ?? ''}
-                                                    autoComplete
-                                                    disableClearable
-                                                    onChange={async (event, state) => {
-                                                        setValue('city', {
-                                                            id: 0,
-                                                            label: '',
-                                                            name: '',
-                                                            state,
-                                                        });
-
-                                                        await trigger('city');
-                                                        await refetchCities();
-                                                    }}
-                                                    options={states ?? []}
-                                                    renderInput={(params) => (
-                                                        <TextField
-                                                            {...params}
-                                                            variant="filled"
-                                                            fullWidth
-                                                            label="Estado"
-                                                            error={!!errors.city?.state}
-                                                            helperText={errors.city?.state?.message}
-                                                        />
-                                                    )}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Autocomplete
-                                                    value={getValues('city') ?? ''}
-                                                    autoComplete
-                                                    disableClearable
-                                                    disabled={isLoadingCities || cities?.length === 0}
-                                                    onChange={async (event, city) => {
-                                                        setValue('city', city);
-
-                                                        await trigger('city');
-                                                    }}
-                                                    options={cities ?? []}
-                                                    renderInput={(params) => (
-                                                        <TextField
-                                                            {...params}
-                                                            variant="filled"
-                                                            fullWidth
-                                                            label="Cidade"
-                                                            error={!!errors.city}
-                                                            helperText={errors.city?.message}
-                                                        />
-                                                    )}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Typography variant="h5">Usuário</Typography>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Controller
-                                                    name="email"
-                                                    control={control}
-                                                    render={({field}) => <TextField {...field} label="E-mail"
-                                                                                    type="email"
-                                                                                    variant="filled"
-                                                                                    fullWidth error={!!errors.email}
-                                                                                    helperText={errors.email?.message}/>}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <LoadingButton fullWidth variant="contained" size="large" type="submit"
-                                                               loading={isUpdatingUser}>
-                                                    Atualizar
-                                                </LoadingButton>
-                                            </Grid>
-                                        </Grid>
-                                    </form>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Container>
-        </>
-    );
+  return (
+      <>
+        <Head>
+          <title>MiAudote - {loadingUser ? 'Carregando...' : user?.name}</title>
+        </Head>
+        <Container maxWidth="sm">
+          <Box paddingY={3}>
+            <Grid container justifyContent="center" alignContent="center"
+                  spacing={2}>
+              <Grid item>
+                <Card>
+                  <CardContent>
+                    {loadingUser ? <CircularProgress/> : null}
+                    {user ? <UserEditForm user={user}/> : null}
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </>
+  );
 };
 
 export default UserShow;
---
+++ src/components/UserEditForm.tsx
@@ -0,0 +1,268 @@
+import {ChangeEvent, FC} from 'react';
+import Alert from '@mui/material/Alert';
+import Autocomplete from '@mui/material/Autocomplete';
+import Grid from '@mui/material/Grid';
+import LoadingButton from '@mui/lab/LoadingButton';
+import PetsIcon from '@mui/icons-material/Pets';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+import {Controller} from 'react-hook-form';
+import {format, parseISO, subYears} from 'date-fns';
+import {AsYouType} from 'libphonenumber-js';
+import {AvatarChangeEvent, InteractableAvatar} from './InteractableAvatar';
+import {User, UserUpdateFieldValues} from '../types';
+import useForm from '../hooks/useForm';
+import * as yup from 'yup';
+import {useUserUpdateMutation} from '../hooks/mutations/useUserUpdateMutation';
+import {
+  useGetCitiesByStateQuery,
+} from '../hooks/queries/useGetCitiesByStateQuery';
+import {useGetStatesQuery} from '../hooks/queries/useGetStatesQuery';
+
+type UserEditFormProps = {
+  user: User,
+};
+
+const minDate = subYears(new Date(), 150);
+const maxDate = subYears(new Date(), 18);
+const stateObject = yup.object({
+  name: yup.string().required(),
+  initials: yup.string().required(),
+  label: yup.string().required(),
+});
+const schema = yup.object({
+  name: yup.string().required('O campo nome é obrigatório.'),
+  bornAt: yup.date().
+      required('O campo data de nascimento é obrigatório.').
+      min(minDate, 'O campo data de nascimento deve ser maior que ' +
+          format(minDate, 'dd/MM/yyyy') + '.').
+      max(maxDate, 'A sua idade deve ser maior que 18 anos.'),
+  email: yup.string().
+      email('O campo e-mail deve ser um endereço de e-mail válido.').
+      required('O campo e-mail é obrigatório.'),
+  phone: yup.string().required('O campo celular é obrigatório.'),
+  city: yup.object({
+    id: yup.number().required(),
+    name: yup.string().required(),
+    label: yup.string().required(),
+    state: stateObject,
+  }).nullable().required('O campo cidade é obrigatório.'),
+});
+
+export const UserEditForm: FC<UserEditFormProps> = (props) => {
+  const {user} = props;
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+    setValue,
+    getValues,
+    setError,
+    trigger,
+    watch,
+  } = useForm<UserUpdateFieldValues>({
+    // @ts-ignore
+    schema,
+    defaultValues: {
+      ...user,
+      bornAt: parseISO(user.bornAtISO),
+    },
+  });
+
+  const {
+    mutate: updateUser,
+    message,
+    isLoading: isUpdatingUser,
+  } = useUserUpdateMutation({setError});
+  const {data: states, isLoading: isLoadingStates} = useGetStatesQuery();
+  const {
+    data: cities,
+    isLoading: isLoadingCities,
+    refetch: refetchCities,
+    isRefetching: refethingCities,
+  } = useGetCitiesByStateQuery({state: getValues('city.state')});
+
+  const loading = isLoadingStates || isUpdatingUser || isLoadingCities ||
+      refethingCities;
+  const onSubmit = handleSubmit(
+      (data: UserUpdateFieldValues) => updateUser(data));
+  const onAvatarChange = async ({file, avatar}: AvatarChangeEvent) => {
+    if (!file || !avatar) {
+      return setError('avatar', {
+        message: 'Invalid upload',
+      });
+    }
+
+    setValue('avatar', avatar);
+    setValue('file', file);
+
+    await trigger(['avatar', 'file']);
+  };
+
+  return (
+      <form onSubmit={onSubmit}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} textAlign="center">
+            <PetsIcon fontSize="large" color="primary"/>
+          </Grid>
+          <Grid item xs={12} justifyContent="center" display="flex">
+            <InteractableAvatar onChange={onAvatarChange} alt={watch('name')}
+                                src={getValues('avatar')}/>
+          </Grid>
+          {message && (
+              <Grid item xs={12}>
+                <Alert severity="error">{message}</Alert>
+              </Grid>
+          )}
+          <Grid item xs={12}>
+            <Typography variant="h5">Dados Pessoais</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+                name="name"
+                control={control}
+                render={({field}) => <TextField {...field} label="Nome"
+                                                variant="filled"
+                                                fullWidth error={!!errors.name}
+                                                helperText={errors.name?.message}/>}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+                name="bornAt"
+                control={control}
+                render={({field}) => {
+                  return <DatePicker
+                      label="Data de Nascimento"
+                      inputFormat="dd/MM/yyyy"
+                      value={getValues('bornAt')}
+                      onChange={(bornAt) => {
+                        if (bornAt) setValue('bornAt', bornAt);
+                      }}
+                      disableFuture
+                      openTo="year"
+                      views={['year', 'month', 'day']}
+                      minDate={minDate}
+                      maxDate={maxDate}
+                      renderInput={(params) => {
+                        const inputProps = {
+                          ...field,
+                          ...params.inputProps,
+                        };
+
+                        return (
+                            <TextField {...params}
+                                       fullWidth
+                                       inputProps={inputProps}
+                                       variant="filled"
+                                       error={!!errors.bornAt}
+                                       helperText={errors.bornAt?.message}/>
+                        );
+                      }}
+                  />;
+                }}/>
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+                name="phone"
+                control={control}
+                render={({field}) => (
+                    <TextField
+                        {...field}
+                        label="Celular"
+                        variant="filled"
+                        fullWidth
+                        error={!!errors.phone}
+                        helperText={errors.phone?.message}
+                        onChange={(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+                          field.onChange(
+                              new AsYouType('BR').input(event.target.value));
+                        }}
+                    />
+                )}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h5">Endereço</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Autocomplete
+                value={getValues('city.state') ?? ''}
+                autoComplete
+                disableClearable
+                disabled={loading}
+                onChange={async (event, state) => {
+                  setValue('city', {
+                    id: 0,
+                    label: '',
+                    name: '',
+                    state,
+                  });
+
+                  await trigger('city');
+                  await refetchCities();
+                }}
+                options={states ?? []}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        variant="filled"
+                        fullWidth
+                        label="Estado"
+                        error={!!errors.city?.state}
+                        helperText={errors.city?.state?.message}
+                    />
+                )}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Autocomplete
+                value={getValues('city') ?? ''}
+                autoComplete
+                disableClearable
+                disabled={loading || cities?.length === 0}
+                onChange={async (event, city) => {
+                  setValue('city', city);
+
+                  await trigger('city');
+                }}
+                options={cities ?? []}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        variant="filled"
+                        fullWidth
+                        label="Cidade"
+                        error={!!errors.city}
+                        helperText={errors.city?.message}
+                    />
+                )}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h5">Usuário</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+                name="email"
+                control={control}
+                render={({field}) => <TextField {...field} label="E-mail"
+                                                type="email"
+                                                variant="filled"
+                                                fullWidth error={!!errors.email}
+                                                helperText={errors.email?.message}/>}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <LoadingButton fullWidth variant="contained" size="large"
+                           type="submit"
+                           loading={loading}>
+              Atualizar
+            </LoadingButton>
+          </Grid>
+        </Grid>
+      </form>
+  );
+};
